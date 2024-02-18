b1ebb9bb50fa45fa69700722bc5f1e5df294d623
stefanusdeo <stefanusdeo07@gmail.com>
2024-02-18 22:21:58 +0700

    fixing connectionDb

--- src/config/connectDb.js
+++ src/config/connectDb.js
@@ -4,6 +4,7 @@ const connection = mysql.createPool({
   connectionLimit: 10,
   host: process.env.NODE_HOST,
   user: process.env.NODE_DATABASE_USERNAME,
+  password: process.env.NODE_DATABASE_PASS,
   port: process.env.NODE_DATABASE_PORT,
   database: process.env.NODE_DATABASE,
 });
